634427c47c2bf1be063ffbd8d63efce58546f66d
mrpark219 <mrpark219@gmail.com>
2024-10-13 21:35:58 +0900

    ✨ 프로그래머스 알고리즘 - 42626번 완료 https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java

--- src/programmers/problem/ProgrammersRunner.java
+++ src/programmers/problem/ProgrammersRunner.java
@@ -4,10 +4,9 @@ public class ProgrammersRunner {
 
 	public static void main(String[] args) {
 
-		int bridge_length = 2;
-		int weight = 10;
-		int[] truck_weights = new int[]{7,4,5,6};
+		int[] scoville = new int[]{7,4,5,6};
+		int K = 7;
 
-		System.out.println(new P42583.Solution().solution(bridge_length, weight, truck_weights));
+		System.out.println(new P42626.Solution().solution(scoville, K));
 	}
 }
