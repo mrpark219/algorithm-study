a2a1a516ddd8f6c462ab37148ad6366fa3917d35
mrpark219 <mrpark219@gmail.com>
2024-10-17 15:46:57 +0900

    ✨ 프로그래머스 알고리즘 - 42576번 완료 https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java

--- src/programmers/problem/ProgrammersRunner.java
+++ src/programmers/problem/ProgrammersRunner.java
@@ -1,14 +1,12 @@
 package programmers.problem;
 
-import java.util.Arrays;
-
 public class ProgrammersRunner {
 
 	public static void main(String[] args) {
 
-		int[] array = {1, 5, 2, 6, 3, 7, 4};
-		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
 
-		System.out.println(Arrays.toString(new P42748.Solution().solution(array, commands)));
+		System.out.println(new P42576.Solution().solution(participant, completion));
 	}
 }
