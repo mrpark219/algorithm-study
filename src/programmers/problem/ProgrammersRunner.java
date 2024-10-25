a658dff635b10af9c5a1f7d8605b7915c709184b
mrpark219 <mrpark219@gmail.com>
2024-10-26 00:43:38 +0900

    ✨ 프로그래머스 알고리즘 - 87946번 완료 https://school.programmers.co.kr/learn/courses/30/lessons/87946

--- src/programmers/problem/ProgrammersRunner.java
+++ src/programmers/problem/ProgrammersRunner.java
@@ -4,9 +4,9 @@ public class ProgrammersRunner {
 
 	public static void main(String[] args) {
 
-		String number = "1924";
-		int k = 2;
+		int k = 80;
+		int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
 
-		System.out.println(new P42883.Solution().solutionStack(number, k));
+		System.out.println(new P87946.Solution().solution(k, dungeons));
 	}
 }
