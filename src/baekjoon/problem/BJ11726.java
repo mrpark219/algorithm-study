bc68658dcf78fd38a6806c02ada002438a2df9a6
mrpark219 <mrpark219@gmail.com>
2024-11-07 10:01:21 +0900

    ✨ 백준 11726번 완료 https://www.acmicpc.net/problem/11726

---
+++ src/baekjoon/problem/BJ11726.java
@@ -0,0 +1,31 @@
+package baekjoon.problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11726 {
+
+	public static Integer[] dp = new Integer[1001];
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		dp[1] = 1;
+		dp[2] = 2;
+
+		System.out.println(solution(n));
+	}
+
+	public static int solution(int n) {
+
+		if(dp[n] == null) {
+			dp[n] = (solution(n - 1) + solution(n - 2)) % 10007;
+		}
+
+		return dp[n];
+	}
+}
